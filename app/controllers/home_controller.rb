cae71c55d24141e78df44e3deff1253ff9d8e011
lmihionkhan <lesley.ihionkhan@chicagobooth.edu>
2022-03-14 13:58:10 +0000

    extracted months from income

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,16 @@
 class HomeController < ApplicationController
   def dashboard
+    require "date"
+
+    #calculate total incomes by month
+
+    matching_incomes = Income.all
+    @list_of_incomes = matching_incomes.order({ :expected_date => :asc })
+
+    #calculate total expenses by month
+    #subtract total expenses from total income for each month to find net cash intake
+    #show steps 1-3 in a table, show step 3 in a graph for each month
+
     render({ :template => "home/home_page.html.erb" })
   end
 end
