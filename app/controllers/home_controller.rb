4fcae782a08e978f234464944f277cce6408df9a
lmihionkhan <lesley.ihionkhan@chicagobooth.edu>
2022-03-14 18:38:20 +0000

    commented out month calculations in controller

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -6,24 +6,25 @@ class HomeController < ApplicationController
 
     ###grab incomes from this user
 
-    @matching_incomes = Income.where(:users_id => @current_user.id)
+    matching_incomes = Income.where(:users_id => @current_user.id)
+    @list_of_incomes = matching_incomes.order({ :created_at => :desc })
 
     ###sum all incomes for each month
 
-    target_month = 1
+    #target_month = 1
 
-    while target_month <= 12
-      @monthly_incomes = @matching_incomes.where({ :expected_date.Date.month => target_month })
+    #while target_month <= 12
+    #@monthly_incomes = @matching_incomes.where({ :expected_date.Date.month => target_month })
 
-      @total_monthly_income = 0
+    #@total_monthly_income = 0
 
-      @monthly_incomes.each do |a_thing|
-        amount = a_thing.amount.to_f
-        @total_monthly_income = amount + @total_monthly_income
-      end
+    #@monthly_incomes.each do |a_thing|
+    #amount = a_thing.amount.to_f
+    #@total_monthly_income = amount + @total_monthly_income
+    #end
 
-      target_month = target_month + 1
-    end
+    #target_month = target_month + 1
+    #end
 
     #calculate total expenses by month
     #subtract total expenses from total income for each month to find net cash intake
