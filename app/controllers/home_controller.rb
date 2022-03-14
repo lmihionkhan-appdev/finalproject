47e6820b42c3425b7c2a86a308e049cbc6d59829
lmihionkhan <lesley.ihionkhan@chicagobooth.edu>
2022-03-14 18:26:56 +0000

    testing sum of all monthly incomes

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -6,8 +6,26 @@ class HomeController < ApplicationController
 
     ###grab incomes from this user
 
-    matching_incomes = Income.where(:users_id => @current_user.id)
-    @list_of_incomes = matching_incomes.order({ :expected_date => :asc })
+    @matching_incomes = Income.where(:users_id => @current_user.id)
+
+    ###sum all incomes for each month
+
+    target_month = 1
+
+    while target_month <= 12
+      @list_of_incomes = @matching_incomes.order({ :expected_date => :asc })
+
+      @monthly_incomes = @list_of_incomes.where(:expected_date.month => target_month)
+
+      @total_monthly_income = 0
+
+      @monthly_incomes.each do |a_thing|
+        amount = a_thing.amount.to_f
+        @total_monthly_income = amount + @total_monthly_income
+      end
+
+      target_month = target_month + 1
+    end
 
     #calculate total expenses by month
     #subtract total expenses from total income for each month to find net cash intake
