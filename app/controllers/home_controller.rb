cbe9e29e230205a4b78597d456441a6951379093
lmihionkhan <lesley.ihionkhan@chicagobooth.edu>
2022-03-17 04:52:05 +0000

    manually coded every months total income and expenses

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -89,6 +89,8 @@ class HomeController < ApplicationController
       end
     end
 
+    @mar_net_cash = @mar_monthly_income - @mar_monthly_expenses
+
     #Apr metrics
 
     @apr_monthly_income = 0
@@ -113,6 +115,8 @@ class HomeController < ApplicationController
       end
     end
 
+    @apr_net_cash = @apr_monthly_income - @apr_monthly_expenses
+
     #May metrics
 
     @may_monthly_income = 0
@@ -136,6 +140,190 @@ class HomeController < ApplicationController
         @may_monthly_expenses = @may_monthly_expenses + @expense_amount
       end
     end
+
+    @may_net_cash = @may_monthly_income - @may_monthly_expenses
+
+    #June metrics
+
+    @jun_monthly_income = 0
+
+    @list_of_incomes.each do |thing|
+      @income_month = thing.expected_date.month
+      @income_amount = thing.amount
+
+      if @income_month == 6
+        @jun_monthly_income = @jun_monthly_income + @income_amount
+      end
+    end
+
+    @jun_monthly_expenses = 0
+
+    @list_of_expenses.each do |thing|
+      @expense_month = thing.expected_date.month
+      @expense_amount = thing.amount
+
+      if @expense_month == 6
+        @jun_monthly_expenses = @jun_monthly_expenses + @expense_amount
+      end
+    end
+
+    @jun_net_cash = @jun_monthly_income - @jun_monthly_expenses
+
+    #July metrics
+
+    @jul_monthly_income = 0
+
+    @list_of_incomes.each do |thing|
+      @income_month = thing.expected_date.month
+      @income_amount = thing.amount
+
+      if @income_month == 7
+        @jul_monthly_income = @jul_monthly_income + @income_amount
+      end
+    end
+
+    @jul_monthly_expenses = 0
+
+    @list_of_expenses.each do |thing|
+      @expense_month = thing.expected_date.month
+      @expense_amount = thing.amount
+
+      if @expense_month == 7
+        @jul_monthly_expenses = @jul_monthly_expenses + @expense_amount
+      end
+    end
+
+    @jul_net_cash = @jul_monthly_income - @jul_monthly_expenses
+
+    #Aug metrics
+
+    @aug_monthly_income = 0
+
+    @list_of_incomes.each do |thing|
+      @income_month = thing.expected_date.month
+      @income_amount = thing.amount
+
+      if @income_month == 8
+        @aug_monthly_income = @aug_monthly_income + @income_amount
+      end
+    end
+
+    @aug_monthly_expenses = 0
+
+    @list_of_expenses.each do |thing|
+      @expense_month = thing.expected_date.month
+      @expense_amount = thing.amount
+
+      if @expense_month == 8
+        @aug_monthly_expenses = @aug_monthly_expenses + @expense_amount
+      end
+    end
+
+    @aug_net_cash = @aug_monthly_income - @aug_monthly_expenses
+
+    #Sept metrics
+
+    @sep_monthly_income = 0
+
+    @list_of_incomes.each do |thing|
+      @income_month = thing.expected_date.month
+      @income_amount = thing.amount
+
+      if @income_month == 9
+        @sep_monthly_income = @sep_monthly_income + @income_amount
+      end
+    end
+
+    @sep_monthly_expenses = 0
+
+    @list_of_expenses.each do |thing|
+      @expense_month = thing.expected_date.month
+      @expense_amount = thing.amount
+
+      if @expense_month == 9
+        @sep_monthly_expenses = @sep_monthly_expenses + @expense_amount
+      end
+    end
+
+    @sep_net_cash = @sep_monthly_income - @sep_monthly_expenses
+
+    #Oct metrics
+
+    @oct_monthly_income = 0
+
+    @list_of_incomes.each do |thing|
+      @income_month = thing.expected_date.month
+      @income_amount = thing.amount
+
+      if @income_month == 10
+        @oct_monthly_income = @oct_monthly_income + @income_amount
+      end
+    end
+
+    @oct_monthly_expenses = 0
+
+    @list_of_expenses.each do |thing|
+      @expense_month = thing.expected_date.month
+      @expense_amount = thing.amount
+
+      if @expense_month == 10
+        @oct_monthly_expenses = @oct_monthly_expenses + @expense_amount
+      end
+    end
+
+    @oct_net_cash = @oct_monthly_income - @oct_monthly_expenses
+
+    #Nov metrics
+
+    @nov_monthly_income = 0
+
+    @list_of_incomes.each do |thing|
+      @income_month = thing.expected_date.month
+      @income_amount = thing.amount
+
+      if @income_month == 11
+        @nov_monthly_income = @nov_monthly_income + @income_amount
+      end
+    end
+
+    @nov_monthly_expenses = 0
+
+    @list_of_expenses.each do |thing|
+      @expense_month = thing.expected_date.month
+      @expense_amount = thing.amount
+
+      if @expense_month == 11
+        @noc_monthly_expenses = @nov_monthly_expenses + @expense_amount
+      end
+    end
+
+    @nov_net_cash = @nov_monthly_income - @nov_monthly_expenses
+
+    #Dec metrics
+
+    @dec_monthly_income = 0
+
+    @list_of_incomes.each do |thing|
+      @income_month = thing.expected_date.month
+      @income_amount = thing.amount
+
+      if @income_month == 12
+        @dec_monthly_income = @dec_monthly_income + @income_amount
+      end
+    end
+
+    @dec_monthly_expenses = 0
+
+    @list_of_expenses.each do |thing|
+      @expense_month = thing.expected_date.month
+      @expense_amount = thing.amount
+
+      if @expense_month == 12
+        @dec_monthly_expenses = @dec_monthly_expenses + @expense_amount
+      end
+    end
+
+    @dec_net_cash = @dec_monthly_income - @dec_monthly_expenses
 
     #####leveraging loops...copy to view page if list needed but formatting didn't work for chart or table
 
